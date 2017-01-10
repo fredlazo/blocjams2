90d7fc264147d9ffe879b992bdb1b9ed9bf28763
Frederick Lazo <fredlazo@yahoo.com>
2017-01-10 15:16:19 -0800

    Checkpoint 24 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,17 +2,15 @@
 
 var pointsArray = document.getElementsByClassName('point');
 
+function revealPoint(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 var animatePoints = function(points) {
-     function revealPoint(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-     for (var i=0; i<points.length; i++){
-         revealPoint(i);
-     }
+    forEach(pointsArray, revealPoint);    
  };
 
 
